66670eeff281f71cbb245252a1df1e0b54f48148
DanielGall500 <daniel.gallagher1@ucdconnect.ie>
2023-02-10 16:53:31 +0000

    meta data improved for fastapi impl

--- src/app/api.py
+++ src/app/api.py
@@ -4,17 +4,36 @@ from app.models import LoginDetails, TranslateStatus, TranslateRequest, Translat
 from app.auth.auth_handler import authenticate_user
 from app.auth.auth_bearer import JWTBearer, signJWT
 
-app = FastAPI()
+app = FastAPI(
+    title="Adapt Translation API",
+    description="This API provides efficient and secure connection to 30 language models comprising of six available language: English, German, French, Italian, Polish, and Irish.",
+    version="2.0",
+    contact={
+        "name": "Adapt Centre, Dublin City University",
+        "url": "https://www.adaptcentre.ie/",
+    },
+    openapi_tags=[
+        {
+            "name": "Translation Calls",
+            "description": "Any routes for calling the translation services. Each language has a specific code associated with it which should be used when passing the JSON parameters. English is 'en', German is 'de', French is 'fr', Italian is 'it', Polish is 'pl', and Irish is 'ga'. In order to make translations, you first need to ensure that you are logged in. Please see the authentication routes for more information."
+        }
+    ]
+)
 
-@app.get("/", tags=["Testing"])
+@app.get("/", tags=["Maintenance"])
 async def root():
-    return RedirectResponse(url="/docs")
+    return RedirectResponse(url="/redoc")
 
+@app.get("/restart", tags=["Maintenance"])
+async def restart():
+    print("Restarting the server...")
+    pass
 
-@app.post("/translate", dependencies=[Depends(JWTBearer())], tags=["Translation Calls"])
+@app.post("/translate", dependencies=[Depends(JWTBearer())], response_model=TranslateResponse, tags=["Translation Calls"])
 async def translate(request: TranslateRequest):
     response = TranslateResponse()
-    response.message = "Translations successful!"
+    response.username = "Daniel"
+    response.status = TranslateStatus()
     return response
 
 @app.post("/login", tags=["User Authentication"])
--- src/app/models.py
+++ src/app/models.py
@@ -1,19 +1,19 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 class LoginDetails(BaseModel):
     username: str
     password: str
 
 class TranslateRequest(BaseModel):
-    src: str
-    tgt: str
-    text: str
+    src: str = Field(description="The source language.")
+    tgt: str = Field(description="The target language.")
+    text: str = Field(description="The text which you want to translate.")
 
-class TranslateStatus:
+class TranslateStatus(BaseModel):
     token_validated: bool = False
     translated: bool = False
     message: str = "Nothing to report."
 
-class TranslateResponse:
-    username: str
-    status: TranslateStatus
+class TranslateResponse(BaseModel):
+    username: str = Field("Unknown")
+    status: TranslateStatus = Field(None)
