69fdba36648e12308f57a22b434da9521f1087eb
DanielGall500 <daniel.gallagher1@ucdconnect.ie>
2023-02-22 16:06:42 +0000

    model time test added

--- src/app/mt_models/connection.py
+++ src/app/mt_models/connection.py
@@ -1,6 +1,8 @@
 from app.mt_models.information import model_info
 from app.mt_models.requests import MTRequestHandler
 import socket
+import time
+
 
 model_translation_test = {
     "en": "Climate change",
@@ -49,26 +51,30 @@ class MTServerConnection:
                 raise socket.error()
 
             # Ensure an OK status is received when connecting to a particular model
+            start_time = time.time()
             translation_test_response = await self.request_handler.translate(src,tgt,model_translation_test[src])
+            translation_test_time = round(time.time() - start_time, 4)
+
             translation_test_output_text = translation_test_response["result"]
             is_model_available = translation_test_response["state"] == self.request_handler.STATUS_OK
         except socket.error:
             is_model_available = False
 
         # Update the new connection status and close the socket
-        self.set_connection_status(src, tgt, is_model_available, translation_test_output_text)
+        self.set_connection_status(src, tgt, is_model_available, translation_test_output_text, translation_test_time)
         lang_pair_socket.close()
         return is_model_available
 
     def all_as_dict(self) -> dict:
         return self.model_connections
 
-    def set_connection_status(self, src: str, tgt: str, is_connected: bool, test_output: str):
+    def set_connection_status(self, src: str, tgt: str, is_connected: bool, test_output: str, test_time: float):
         key = self._convert_pair_to_key(src, tgt)
         self.model_connections[key] = is_connected
         self.model_connections[key] = {
             "is_connected": is_connected,
-            "output": test_output
+            "output": test_output,
+            "time": test_time
         }
 
     def get_connection_status(self, src: str, tgt: str) -> dict:
